eddc218ef4e205b58444b73e1f7dd37af3e9a178
PatelPratik39 <ptl.pratik717@gmail.com>
2024-10-11 14:09:32 -0500

    table configured

--- client-app/UrlShortner/src/components/Container/Container.tsx
+++ client-app/UrlShortner/src/components/Container/Container.tsx
@@ -1,11 +1,32 @@
 import * as React from 'react';
 import FormContainer from '../FormContainer/FormContainer';
+import { UrlData } from '../../interface/UrlData';
+import axios from 'axios';
+import { serverUrl } from '../../helper/constant';
+import DataTable from '../DataTable/Datatable';
 
 interface IContainerProps {
+
 }
 
 const Container: React.FunctionComponent<IContainerProps> = () => {
-    return <div><FormContainer /></div>
+
+    const [data, setData] = React.useState<UrlData[]>([]);
+
+    const fetchTableData = async () => {
+        const resposne = await axios.get(`${serverUrl}/shortUrl`)
+        console.log("The Response from server is : ", resposne);
+        setData(resposne.data)
+        // console.log("Data : ", data);
+    }
+
+    React.useEffect(() => {
+        fetchTableData();
+    }, [])
+
+
+    return <div><FormContainer />
+        <DataTable data={data}/></div>
 };
 
 export default Container;
