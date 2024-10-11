ef96f9a6a84339b4a67a1980a239ec1adaf91387
PatelPratik39 <ptl.pratik717@gmail.com>
2024-10-11 14:25:49 -0500

    copy url works

--- client-app/UrlShortner/src/components/DataTable/Datatable.tsx
+++ client-app/UrlShortner/src/components/DataTable/Datatable.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { UrlData } from '../../interface/UrlData';
 import { Link } from 'react-router-dom';
 import { serverUrl } from '../../helper/constant';
+import axios from 'axios';
 
 interface IDataTableProps {
     data: UrlData[];
@@ -10,7 +11,6 @@ interface IDataTableProps {
 const DataTable: React.FunctionComponent<IDataTableProps> = (props) => {
     const { data } = props;
     console.log("Data is datatable", data);
-
 
     const renderTableData = () => {
         return data.map((item) => {
@@ -30,20 +30,67 @@ const DataTable: React.FunctionComponent<IDataTableProps> = (props) => {
                         {item.clicks}
                     </td>
                     <td className='px-6 py-3 '>
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-                            <path strokeLinecap="round" strokeLinejoin="round" d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 0 0 2.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 0 0-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75 2.25 2.25 0 0 0-.1-.664m-5.8 0A2.251 2.251 0 0 1 13.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25ZM6.75 12h.008v.008H6.75V12Zm0 3h.008v.008H6.75V15Zm0 3h.008v.008H6.75V18Z" />
-                            <path strokeLinecap="round" strokeLinejoin="round" d="m20.25 7.5-.625 10.632a2.25 2.25 0 0 1-2.247 2.118H6.622a2.25 2.25 0 0 1-2.247-2.118L3.75 7.5m6 4.125 2.25 2.25m0 0 2.25 2.25M12 13.875l2.25-2.25M12 13.875l-2.25 2.25M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125Z" />
-                        </svg>
-
-
-
-
+                        <div className="flex content-center">
+                            <div
+                                className="cursor-pointer px-2"
+                                onClick={() => copyToClipboard(item.shortUrl)}
+                            >
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    viewBox="0 0 24 24"
+                                    fill="currentColor"
+                                    className="w-6 h-6 fill-white-600"
+                                >
+                                    <path
+                                        fillRule="evenodd"
+                                        d="M7.502 6h7.128A3.375 3.375 0 0 1 18 9.375v9.375a3 3 0 0 0 3-3V6.108c0-1.505-1.125-2.811-2.664-2.94a48.972 48.972 0 0 0-.673-.05A3 3 0 0 0 15 1.5h-1.5a3 3 0 0 0-2.663 1.618c-.225.015-.45.032-.673.05C8.662 3.295 7.554 4.542 7.502 6ZM13.5 3A1.5 1.5 0 0 0 12 4.5h4.5A1.5 1.5 0 0 0 15 3h-1.5Z"
+                                        clipRule="evenodd"
+                                    />
+                                    <path
+                                        fillRule="evenodd"
+                                        d="M3 9.375C3 8.339 3.84 7.5 4.875 7.5h9.75c1.036 0 1.875.84 1.875 1.875v11.25c0 1.035-.84 1.875-1.875 1.875h-9.75A1.875 1.875 0 0 1 3 20.625V9.375Zm9.586 4.594a.75.75 0 0 0-1.172-.938l-2.476 3.096-.908-.907a.75.75 0 0 0-1.06 1.06l1.5 1.5a.75.75 0 0 0 1.116-.062l3-3.75Z"
+                                        clipRule="evenodd"
+                                    />
+                                </svg>
+                            </div>
+                            <div
+                                className="cursor-pointer px-2"
+                                onClick={() => deleteUrl(item._id)}
+                            >
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    viewBox="0 0 24 24"
+                                    fill="currentColor"
+                                    className="w-6 h-6 fill-red-500"
+                                >
+                                    <path
+                                        fillRule="evenodd"
+                                        d="M16.5 4.478v.227a48.816 48.816 0 0 1 3.878.512.75.75 0 1 1-.256 1.478l-.209-.035-1.005 13.07a3 3 0 0 1-2.991 2.77H8.084a3 3 0 0 1-2.991-2.77L4.087 6.66l-.209.035a.75.75 0 0 1-.256-1.478A48.567 48.567 0 0 1 7.5 4.705v-.227c0-1.564 1.213-2.9 2.816-2.951a52.662 52.662 0 0 1 3.369 0c1.603.051 2.815 1.387 2.815 2.951Zm-6.136-1.452a51.196 51.196 0 0 1 3.273 0C14.39 3.05 15 3.684 15 4.478v.113a49.488 49.488 0 0 0-6 0v-.113c0-.794.609-1.428 1.364-1.452Zm-.355 5.945a.75.75 0 1 0-1.5.058l.347 9a.75.75 0 1 0 1.499-.058l-.346-9Zm5.48.058a.75.75 0 1 0-1.498-.058l-.347 9a.75.75 0 0 0 1.5.058l.345-9Z"
+                                        clipRule="evenodd"
+                                    />
+                                </svg>
+                            </div>
+                        </div>
                     </td>
                 </tr>
             )
         })
 
     }
+
+    const copyToClipboard = async (url: string) => {
+        try {
+            await navigator.clipboard.writeText(`${serverUrl}/shortUrl/${url}`)
+            alert(`URL copied : ${serverUrl}/shortUrl/${url} `)
+        } catch (error) {
+            console.error(error);
+        }
+    }
+    const deleteUrl = async (id: string) => {
+        const response = await axios.delete(`${serverUrl}/shortUrl/${id}`);
+        console.log(response);
+        // updateReloadState();
+    };
     return (
         <div className='conatainer mx-auto pt-2 pb-10'>
             <div className='relative overflow-x-auto shadow-sm sm:rounded-lg'>
